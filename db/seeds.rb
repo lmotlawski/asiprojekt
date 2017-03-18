08bbeec686f75e4b9bb3f40b3e2a6bf2d2dc1753
Arkadiusz Półgęsek <kontakt@arkadiuszpolgesek.com>
2017-03-18 17:59:45 +0100

    Wczytywanie danych z pliku

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+File.open("db/db.txt", "r") do |f|
+  f.each_line do |line|
+    nazwa, opis, url = line.chomp.split(";")
+    Katalog.create!(:nazwa => nazwa, :opis => opis, :url => url)
+  end
+end

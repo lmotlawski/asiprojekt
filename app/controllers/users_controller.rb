0167e9cd43fcd1bb51ffb628badc29d1768aaec6
Arkadiusz Półgęsek <kontakt@arkadiuszpolgesek.com>
2017-03-19 02:10:50 +0100

    Walidacja formularzy

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,19 +3,31 @@ class UsersController < ApplicationController
   def new
   end
 
-  def create
-    user = User.new(user_params)
-    if user.save
-      session[:user_id] = user.id
-      redirect_to '/'
+def create
+
+    if User.find_by_email(params[:user][:email])
+
+      flash.now[:danger] = 'Użytkownik o podanym adresie już istnieje!'
+      render 'new'
+
     else
-      redirect_to '/signup'
+
+    user = User.new(user_params)
+
+      if user.save
+        session[:user_id] = user.id
+        redirect_to '/'
+      else
+        redirect_to '/signup'
+      end
+
     end
-  end
+
+end
 
 private
 
   def user_params
-    params.require(:user).permit(:login, :email, :password, :password_confirmation)
+    params.require(:user).permit(:email, :password, :password_confirmation)
   end
 end
